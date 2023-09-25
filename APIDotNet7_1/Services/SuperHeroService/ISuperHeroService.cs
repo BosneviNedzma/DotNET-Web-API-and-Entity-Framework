69c561b92d69c4ecaf2ca52aedcb35b4b5da8db2
Nedzma <brkicnedzma@gmail.com>
2023-09-25 16:38:48 +0200

    Add project files.

---
+++ APIDotNet7_1/Services/SuperHeroService/ISuperHeroService.cs
@@ -0,0 +1,11 @@
+﻿namespace APIDotNet7_1.Services.SuperHeroService
+{
+    public interface ISuperHeroService
+    {
+        Task<List<SuperHero>> GetAllHeroes();
+        Task<SuperHero?> GetSingleHero(int id);
+        Task<List<SuperHero>>AddHero(SuperHero hero);
+        Task<SuperHero?> UpdateHero(int id, SuperHero request);
+        Task<List<SuperHero>?> DeleteHero(int id);
+    }
+}
